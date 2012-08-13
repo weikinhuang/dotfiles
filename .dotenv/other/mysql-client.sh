357d012ab30ca32260fb08c30bf141b71198af8a
Wei Kin Huang <weikin.huang04@gmail.com>
2012-08-13 18:25:20 -0400

    Adding mysql client settings and a alias for mysql to use less as the pager

---
+++ .dotenv/other/mysql-client.sh
@@ -0,0 +1,5 @@
+# custom mysql prompt
+export MYSQL_PS1='\u@\h [\d]'$(echo -e "\xe2\x86\x92")' '
+
+# set pager for mysql client
+alias mysql="mysql --line-numbers --pager='less -inSFX' --show-warnings"
