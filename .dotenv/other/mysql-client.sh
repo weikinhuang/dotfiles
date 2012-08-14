42e0fc7fbd33b5a83c5af8491b949f7d8d891665
Wei Kin Huang <weikin.huang04@gmail.com>
2012-08-13 21:31:58 -0400

    adding a basic mongorc file

--- .dotenv/other/mysql-client.sh
+++ .dotenv/other/mysql-client.sh
@@ -2,4 +2,4 @@
 export MYSQL_PS1='\u@\h [\d]'$(echo -e "\xe2\x86\x92")' '
 
 # set pager for mysql client
-alias mysql="mysql --line-numbers --pager='less -inSFX' --show-warnings"
+alias mysql="mysql --line-numbers --pager='less -inSFX' --show-warnings --default-character-set=utf8"
---
+++ .mongorc.js
@@ -0,0 +1,27 @@
+﻿// generate a MONGO_PS1 DB_STATE#host[dbname]> 
+(function() {
+	"use strict";
+	var states = [ "STARTUP", "PRIMARY", "SECONDARY", "RECOVERING", "FATAL", "STARTUP2", "UNKNOWN", "ARBITER", "DOWN", "ROLLBACK" ];
+	var host = db.serverStatus().host;
+
+	prompt = function() {
+		var dbState, status = "", dbStatus = db.isMaster();
+		if (dbStatus.setName) {
+			if (dbStatus.ismaster) {
+				dbState = 'PRIMARY';
+			} else if (dbStatus.secondary) {
+				dbState = 'SECONDARY';
+			} else {
+				dbState = states[rs.status().myState];
+			}
+			if(dbStatus.hidden) {
+				dbState = dbState.toLowerCase();
+			}
+			status = dbState + ":[" + dbStatus.setName + "]#";
+		}
+		return status + host + "[" + db + "]> ";
+	};
+})();
+
+// always enable pretty print
+DBQuery.prototype._prettyShell = true;
