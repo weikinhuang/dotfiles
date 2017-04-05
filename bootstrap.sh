4dfcc5a8a63d677b4c103ae10a07890f393a8269
Wei Kin Huang <weikin.huang04@gmail.com>
2017-04-05 17:09:25 -0400

    Restore curl bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,14 +4,10 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# sanity check, git is now required for auto install
-if ! type git &>/dev/null; then
-  exit 1
-fi
-
 # variables
-readonly REPO_BASE="${REPO_BASE:-https://github.com/weikinhuang/dotfiles.git}"
-INSTALL_ROOT="${INSTALL_ROOT:-${HOME}}"
+readonly CONFIG_FILE="${HOME}/.config/dotfiles/.install"
+readonly REPO_BASE="${REPO_BASE:-https://github.com/weikinhuang/dotfiles}"
+DOTFILES__INSTALL_ROOT="${DOTFILES__INSTALL_ROOT:-${HOME}}"
 LINKED_FILES=(
   "bash_profile.sh .bash_profile"
   "bashrc.sh .bashrc"
@@ -22,37 +18,48 @@ LINKED_FILES=(
   "screenrc .screenrc"
   "wgetrc .wgetrc"
 )
-INSTALL_VIMRC=1
-INSTALL_GITCONFIG=1
+DOTFILES__INSTALL_VIMRC=1
+DOTFILES__INSTALL_GITCONFIG=1
+
+# pull info from previous installation
+if [[ -e "${CONFIG_FILE}" ]]; then
+  source "${CONFIG_FILE}"
+fi
 
 # link up gitconfig and vim if specified
 if [[ -n $# ]]; then
   for arg in "$@"; do
     case "${arg}" in
       --no-git)
-        INSTALL_GITCONFIG=0
+        DOTFILES__INSTALL_GITCONFIG=0
         ;;
       --no-vim)
-        INSTALL_VIMRC=0
+        DOTFILES__INSTALL_VIMRC=0
         ;;
       --dir|-d)
         shift 1
-        INSTALL_ROOT="${1}"
+        DOTFILES__INSTALL_ROOT="${1}"
         ;;
     esac
   done
 fi
 
 # clean up
-if [[ "${INSTALL_VIMRC}" -eq 1 ]]; then
+if [[ "${DOTFILES__INSTALL_VIMRC}" -eq 1 ]]; then
   LINKED_FILES+=("vimrc .vimrc" "vim .vim")
 fi
-if [[ "${INSTALL_GITCONFIG}" -eq 1  ]]; then
+if [[ "${DOTFILES__INSTALL_GITCONFIG}" -eq 1  ]]; then
   LINKED_FILES+=("gitconfig .gitconfig")
 fi
 
 # root directory to install into
-readonly DOTFILES_ROOT="${INSTALL_ROOT}/.dotfiles"
+readonly DOTFILES_ROOT="${DOTFILES__INSTALL_ROOT}/.dotfiles"
+
+# store the configuration of for future reference
+mkdir -p "$(dirname "${CONFIG_FILE}")"
+echo "DOTFILES__INSTALL_ROOT=\"${DOTFILES__INSTALL_ROOT}\"" > "${CONFIG_FILE}"
+echo "DOTFILES__INSTALL_VIMRC=${DOTFILES__INSTALL_VIMRC}" >> "${CONFIG_FILE}"
+echo "DOTFILES__INSTALL_GITCONFIG=${DOTFILES__INSTALL_GITCONFIG}" >> "${CONFIG_FILE}"
 
 # link up files
 function dotfiles::link () {
@@ -60,18 +67,58 @@ function dotfiles::link () {
   local target="$(echo "${1}" | cut -d' ' -f2)"
 
   # remove the backup first
-  [[ -e "${INSTALL_ROOT}/${target}.bak" ]] && rm -f "${INSTALL_ROOT}/${target}.bak"
+  [[ -e "${DOTFILES__INSTALL_ROOT}/${target}.bak" ]] && rm -f "${DOTFILES__INSTALL_ROOT}/${target}.bak"
 
   # create the backup file if not symlink
-  [[ -e "${INSTALL_ROOT}/${target}" && ! -L "${INSTALL_ROOT}/${target}" ]] \
-    && mv "${INSTALL_ROOT}/${target}" "${INSTALL_ROOT}/${target}.bak"
+  [[ -e "${DOTFILES__INSTALL_ROOT}/${target}" && ! -L "${DOTFILES__INSTALL_ROOT}/${target}" ]] \
+    && mv "${DOTFILES__INSTALL_ROOT}/${target}" "${DOTFILES__INSTALL_ROOT}/${target}.bak"
 
   # link up the file
   if [[ -e "${DOTFILES_ROOT}/${file}" ]]; then
-    echo "linking up '${DOTFILES_ROOT}/${file}' => '${INSTALL_ROOT}/${target}'"
-    if ! ln -sf "${DOTFILES_ROOT}/${file}" "${INSTALL_ROOT}/${target}"; then
-      echo "Unable to symlink '${INSTALL_ROOT}/${target}'"
+    echo "linking up '${DOTFILES_ROOT}/${file}' => '${DOTFILES__INSTALL_ROOT}/${target}'"
+    if ! ln -sf "${DOTFILES_ROOT}/${file}" "${DOTFILES__INSTALL_ROOT}/${target}"; then
+      echo "Unable to symlink '${DOTFILES__INSTALL_ROOT}/${target}'"
     fi
+  fi
+}
+
+# abstract updating from git or curl
+function dotfiles::repo::get::git () {
+  git clone "${REPO_BASE}" "${1}"
+}
+
+function dotfiles::repo::get::curl () {
+  curl -#L "$REPO_BASE/tarball/master" | tar -C "${1}" -xzv --strip-components 1
+}
+
+function dotfiles::repo::get () {
+  # sanity check, git is now required for auto install
+  if type git &>/dev/null; then
+    dotfiles::repo::get::git "${DOTFILES_ROOT}"
+  else
+    dotfiles::repo::get::curl "${DOTFILES_ROOT}"
+  fi
+}
+
+function dotfiles::repo::update::git () {
+  # check if there are git changes
+  if git -C "${1}" diff-index --quiet HEAD -- &> /dev/null; then
+    # no changes
+    git -C "${1}" pull origin master || return 1
+  else
+    # changes
+    git -C "${1}" stash || true
+    git -C "${1}" pull origin master || return 1
+    git -C "${1}" stash pop || true
+  fi
+}
+
+function dotfiles::repo::update () {
+  # sanity check, git is now required for auto install
+  if type git &>/dev/null; then
+    dotfiles::repo::update::git "${DOTFILES_ROOT}"
+  else
+    dotfiles::repo::get::curl "${DOTFILES_ROOT}"
   fi
 }
 
@@ -80,19 +127,12 @@ function dotfiles::install () {
   # attempt to make the dotfiles directory
   mkdir -p "${DOTFILES_ROOT}" || return 1
 
-  # do all of our work in here
-  cd "${DOTFILES_ROOT}" || return 1
-
   # download the latest version
-  #git clone "${REPO_BASE}" . || return 1
-  cp -R ${REPO_BASE}/* .
-  cp -R ${REPO_BASE}/.git .
+  dotfiles::repo::get
 
   # Install vundle
-  if [[ "${INSTALL_VIMRC}" -eq 1 ]]; then
-    cd "${DOTFILES_ROOT}/vim/bundle"
-    git clone https://github.com/VundleVim/Vundle.vim.git
-    cd "${DOTFILES_ROOT}"
+  if [[ "${DOTFILES__INSTALL_VIMRC}" -eq 1 ]] && type git &>/dev/null; then
+    git clone https://github.com/VundleVim/Vundle.vim.git "${DOTFILES_ROOT}/vim/bundle/Vundle.vim"
     if type vim &>/dev/null; then
       echo "--------------- Please Run: 'vim +BundleInstall +qall' after installation"
     fi
@@ -101,34 +141,18 @@ function dotfiles::install () {
 
 # update the dotfiles
 function dotfiles::update () {
-  # do all of our work in here
-  cd "${DOTFILES_ROOT}" || return 1
-
-  # check if there are git changes
-  if git diff-index --quiet HEAD -- &> /dev/null; then
-    # no changes
-    git pull origin master || return 1
-  else
-    # changes
-    git stash || true
-    git pull origin master || return 1
-    git stash pop || true
-  fi
+  ## update to the latest version
+  dotfiles::repo::update
 
   # Update vundle
-  if [[ -e "${DOTFILES_ROOT}/vim/bundle/Vundle.vim" ]]; then
-    cd "${DOTFILES_ROOT}/vim/bundle/Vundle.vim"
-    git pull origin master
-    cd "${DOTFILES_ROOT}"
+  if [[ -e "${DOTFILES_ROOT}/vim/bundle/Vundle.vim" ]] && type git &>/dev/null; then
+    git -C "${DOTFILES_ROOT}/vim/bundle/Vundle.vim" pull origin master
     vim +PluginInstall +qall
     if type vim &>/dev/null; then
       echo "--------------- Please Run: 'vim +BundleInstall +qall' after installation"
     fi
   fi
 }
-
-# we want to install this in the home directory
-cd "${INSTALL_ROOT}"
 
 # make the dotfiles directory
 if [[ ! -d "${DOTFILES_ROOT}" ]]; then
