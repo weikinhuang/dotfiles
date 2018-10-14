b3c996659debf630e875ee3b4c6f21f77cc324b2
Wei Kin Huang <weikin.huang04@gmail.com>
2018-10-14 18:26:15 -0400

    wsl git wrapper for vscode

--- utils/wsl/native-wrappers/git.bat
+++ utils/wsl/native-wrappers/git.bat
@@ -1,20 +1,16 @@
 @echo off
 
-REM @see https://gist.github.com/jmickela/7c383c78af66a37a2446fe7eb733b157
+set SCRIPT_PATH=%~dp0
+set SCRIPT_PATH_ESCAPE=%SCRIPT_PATH:\=\\%
 
-setlocal enabledelayedexpansion
-set command=%*
-
-if exist "C:\Users\%USERNAME%\AppData\Local\Temp\git-commit-msg-.txt" (
-for /f "delims=" %%i in ('type C:\Users\%USERNAME%\AppData\Local\Temp\git-commit-msg-.txt') do set commitmsg=!content! %%i
+If %PROCESSOR_ARCHITECTURE% == x86 (
+  for /f "delims=" %%i in ('C:\Windows\Sysnative\wsl.exe wslpath -ua "%SCRIPT_PATH_ESCAPE%"') do set "SCRIPT_PATH_WSL=%%i"
+) Else (
+  for /f "delims=" %%i in ('wsl.exe wslpath -ua "%SCRIPT_PATH_ESCAPE%"') do set "SCRIPT_PATH_WSL=%%i"
 )
 
-set find=-F C:\Users\%USERNAME%\AppData\Local\Temp\git-commit-msg-.txt
-set replace=-m "%commitmsg%"
-
-call set command=%%command:!find!=!replace!%%
 If %PROCESSOR_ARCHITECTURE% == x86 (
-    C:\Windows\Sysnative\wsl.exe bash -c 'git %command%'
+  C:\Windows\Sysnative\wsl.exe %SCRIPT_PATH_WSL%/wslgit %*
 ) Else (
-    C:\Windows\System32\wsl.exe bash -c 'git %command%'
+  C:\Windows\System32\wsl.exe %SCRIPT_PATH_WSL%/wslgit %*
 )
