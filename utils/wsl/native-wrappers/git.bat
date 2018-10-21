d6f02d552ec2cba89c3874caed25d7453b2f622e
Wei Kin Huang <weikin.huang04@gmail.com>
2018-10-21 16:31:33 -0400

    wsl: properly escape backslash in the wslgit bat file

--- utils/wsl/native-wrappers/git.bat
+++ utils/wsl/native-wrappers/git.bat
@@ -1,5 +1,7 @@
 @echo off
 
+SETLOCAL ENABLEDELAYEDEXPANSION
+
 set SCRIPT_PATH=%~dp0
 set SCRIPT_PATH_ESCAPE=%SCRIPT_PATH:\=\\%
 
@@ -9,8 +11,11 @@ If %PROCESSOR_ARCHITECTURE% == x86 (
   for /f "delims=" %%i in ('wsl.exe wslpath -ua "%SCRIPT_PATH_ESCAPE%"') do set "SCRIPT_PATH_WSL=%%i"
 )
 
+set PARAMS=%*
+set ESCAPED_PARAMS=%PARAMS:\=\\%
+
 If %PROCESSOR_ARCHITECTURE% == x86 (
-  C:\Windows\Sysnative\wsl.exe %SCRIPT_PATH_WSL%/wslgit %*
+  C:\Windows\Sysnative\wsl.exe %SCRIPT_PATH_WSL%/wslgit %ESCAPED_PARAMS%
 ) Else (
-  C:\Windows\System32\wsl.exe %SCRIPT_PATH_WSL%/wslgit %*
+  C:\Windows\System32\wsl.exe %SCRIPT_PATH_WSL%/wslgit %ESCAPED_PARAMS%
 )
