531cc74c9f4a95717d45d21c5af97fd195b12263
Wei Kin Huang <weikin.huang04@gmail.com>
2019-08-02 11:38:57 -0400

    wsl: Wrapper script for using wsl ssh (mainly for vscode)

---
+++ utils/wsl/native-wrappers/ssh.bat
@@ -0,0 +1,19 @@
+@echo off
+
+set SCRIPT_PATH=%~dp0
+set SCRIPT_PATH_ESCAPE=%SCRIPT_PATH:\=\\%
+
+If %PROCESSOR_ARCHITECTURE% == x86 (
+  for /f "delims=" %%i in ('C:\Windows\Sysnative\wsl.exe wslpath -ua "%SCRIPT_PATH_ESCAPE%"') do set "SCRIPT_PATH_WSL=%%i"
+) Else (
+  for /f "delims=" %%i in ('wsl.exe wslpath -ua "%SCRIPT_PATH_ESCAPE%"') do set "SCRIPT_PATH_WSL=%%i"
+)
+
+set PARAMS=%*
+set ESCAPED_PARAMS=%PARAMS:\=\\%
+
+If %PROCESSOR_ARCHITECTURE% == x86 (
+  C:\Windows\Sysnative\wsl.exe %SCRIPT_PATH_WSL%/wslgit %ESCAPED_PARAMS%
+) Else (
+  C:\Windows\System32\wsl.exe %SCRIPT_PATH_WSL%/wslgit %ESCAPED_PARAMS%
+)
