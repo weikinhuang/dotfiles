4c912fa369013b1bc153d55ca3d1a6d2d25904c1
Wei Kin Huang <weikin.huang04@gmail.com>
2019-08-02 17:55:54 -0400

    wsl: Wrapper script for using wsl ssh (mainly for vscode)

--- utils/wsl/native-wrappers/ssh.bat
+++ utils/wsl/native-wrappers/ssh.bat
@@ -1,19 +1,12 @@
 @echo off
 
-set SCRIPT_PATH=%~dp0
-set SCRIPT_PATH_ESCAPE=%SCRIPT_PATH:\=\\%
+set v_params=%*
+set v_params=%v_params:\=/%
+set v_params=%v_params:c:=/mnt/c%
+REM set v_params=%v_params:"=\"%
 
 If %PROCESSOR_ARCHITECTURE% == x86 (
-  for /f "delims=" %%i in ('C:\Windows\Sysnative\wsl.exe wslpath -ua "%SCRIPT_PATH_ESCAPE%"') do set "SCRIPT_PATH_WSL=%%i"
+  C:\Windows\Sysnative\wsl.exe ssh %v_params%
 ) Else (
-  for /f "delims=" %%i in ('wsl.exe wslpath -ua "%SCRIPT_PATH_ESCAPE%"') do set "SCRIPT_PATH_WSL=%%i"
-)
-
-set PARAMS=%*
-set ESCAPED_PARAMS=%PARAMS:\=\\%
-
-If %PROCESSOR_ARCHITECTURE% == x86 (
-  C:\Windows\Sysnative\wsl.exe %SCRIPT_PATH_WSL%/wslgit %ESCAPED_PARAMS%
-) Else (
-  C:\Windows\System32\wsl.exe %SCRIPT_PATH_WSL%/wslgit %ESCAPED_PARAMS%
+  C:\Windows\System32\wsl.exe ssh %v_params%
 )
