7525c9320b62e9c4e29dc0dbf82b2a6fbd754596
Wei Kin Huang <weikin.huang04@gmail.com>
2019-11-10 13:53:01 -0500

    Check if we can read /proc/loadavg for prompt

--- dotenv/linux/prompt.sh
+++ dotenv/linux/prompt.sh
@@ -1,4 +1,10 @@
 # function to get cpu load
-function __ps1_proc_use() {
-  echo -n "$(\sed -n "s/\([0-9]\.[0-9][0-9]\).\+/\1/p" /proc/loadavg)"
-}
+if [[ -r /proc/loadavg ]]; then
+  function __ps1_proc_use() {
+    echo -n "$(\sed -n "s/\([0-9]\.[0-9][0-9]\).\+/\1/p" /proc/loadavg)"
+  }
+else
+  function __ps1_proc_use() {
+    echo -n "$(uptime | rev | cut -d' ' -f 3 | rev | \sed -n "s/\([0-9]\.[0-9][0-9]\).\+/\1/p")"
+  }
+fi
