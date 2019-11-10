61e75119ee8a89e761293f688a82b1f41dd2f0cf
Wei Kin Huang <weikin.huang04@gmail.com>
2019-11-10 15:55:49 -0500

    Add script to setup termux

---
+++ utils/termux/setup-termux.sh
@@ -0,0 +1,22 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+pkg upgrade
+
+pkg install \
+  bash-completion \
+  ca-certificates \
+  coreutils \
+  curl \
+  diffutils \
+  direnv \
+  findutils \
+  git \
+  net-tools \
+  openssh \
+  openssl \
+  openssl-tool \
+  perl \
+  ripgrep \
+  rsync \
+  termux-api \
+  vim
