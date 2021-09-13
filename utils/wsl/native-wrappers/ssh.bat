ed86c6d6aacb8b8af0ffcbaca22e811e93d3d262
Wei Kin Huang <weikin.huang04@gmail.com>
2021-09-13 08:11:27 -0400

    wsl: update ssh native-wrapper

--- utils/wsl/native-wrappers/ssh.bat
+++ utils/wsl/native-wrappers/ssh.bat
@@ -66,9 +66,7 @@ set v_params=%v_params:c:=/mnt/c%
 REM set v_params=%v_params:"=\"%
 
 If %PROCESSOR_ARCHITECTURE% == x86 (
-  :: C:\Windows\Sysnative\wsl.exe %~n0 %v_params%
-  C:\Windows\Sysnative\bash.exe -ic "%~n0 %v_params%"
+  C:\Windows\Sysnative\wsl.exe bash -ic "exec %~n0 %v_params%"
 ) Else (
-  :: C:\Windows\System32\wsl.exe %~n0 %v_params%
-  C:\Windows\system32\bash.exe -ic "%~n0 %v_params%"
+  C:\Windows\system32\wsl.exe bash -ic "exec %~n0 %v_params%"
 )
