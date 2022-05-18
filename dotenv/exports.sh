f4561d65bd9588b8bf20e80a161864019f856237
Wei Kin Huang <weikin.huang04@gmail.com>
2022-05-18 12:00:33 -0400

    exports: more highlights in man pages

--- dotenv/exports.sh
+++ dotenv/exports.sh
@@ -37,8 +37,17 @@ export HISTFILESIZE=$HISTSIZE
 export MANPAGER="less -iFXRS -x4"
 # Highlight section titles in manual pages if possible
 if tput setaf 1 &>/dev/null; then
-  LESS_TERMCAP_md="$(tput bold)$(tput setaf 33)"
-  export LESS_TERMCAP_md
+  : "${LESS_TERMCAP_mb:=$'\e[1;32m'}"
+  : "${LESS_TERMCAP_md:=$'\e[1;32m'}"
+  : "${LESS_TERMCAP_me:=$'\e[0m'}"
+  : "${LESS_TERMCAP_se:=$'\e[0m'}"
+  : "${LESS_TERMCAP_so:=$'\e[01;33m'}"
+  : "${LESS_TERMCAP_ue:=$'\e[0m'}"
+  : "${LESS_TERMCAP_us:=$'\e[1;4;31m'}"
+
+  : "${LESS:=}"
+  export "${!LESS_TERMCAP@}"
+  export LESS="R${LESS#-}"
 fi
 
 # set $EDITOR to vi(m) if not already set
