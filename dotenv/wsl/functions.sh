941983e0de6207485f07bcacbcd67db5303f7130
Wei Kin Huang <weikin.huang04@gmail.com>
2022-05-26 15:15:33 -0400

    wsl: pull functions into scripts, remove is-drvfs/volfs-readable

--- dotenv/wsl/functions.sh
+++ dotenv/wsl/functions.sh
@@ -1,38 +1,7 @@
 # shellcheck shell=bash
-# get the windows username
-function winwhoami() {
-  # shellcheck disable=SC2016
-  powershell.exe -c '$env:USERNAME' </dev/null | tr -d '\r'
-}
-
-# check if this is a wsl path
-function is-volfs-readable() {
-  /bin/wslpath -w "$1" &>/dev/null
-}
-export -f is-volfs-readable
-
-# check if this is a windows path
-function is-drvfs-readable() {
-  /bin/wslpath -u "$1" &>/dev/null
-}
-export -f is-drvfs-readable
 
 # run a command under the windows shell
 function cmd0() {
   cmd.exe /c "$@" | sed 's/\r$//'
   return "${PIPESTATUS[0]}"
 }
-
-# checks if the current process (sesison) is running with Administrator privileges.
-function is-elevated-session() {
-  local ret out
-
-  # try using powershell to determine elevated status
-  out="$(powershell.exe -c '(New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)' </dev/null | tr -d '\r')"
-  ret=$?
-  if [[ ${ret} -ne 0 ]]; then
-    return 255
-  fi
-  test "${out}" == "True"
-}
-export -f is-elevated-session
