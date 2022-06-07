8511076bd37f460909f423b78d82af4db911bbc2
Wei Kin Huang <weikin.huang04@gmail.com>
2022-06-07 16:43:29 -0400

    linux: use script instead of alias for open, lower priority in path in case open cmd exists

--- dotenv/linux/aliases.sh
+++ dotenv/linux/aliases.sh
@@ -1,14 +1,5 @@
 # shellcheck shell=bash
 
-# open command shortcuts
-if command -v xdg-open &>/dev/null; then
-  alias open="xdg-open"
-elif command -v gnome-open &>/dev/null; then
-  alias open="gnome-open"
-else
-  alias open="nautilus"
-fi
-
 # Alias vi to vim
 if command -v vim &>/dev/null; then
   alias vi="vim"
