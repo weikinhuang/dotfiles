4bbe083c73d4131fe080663bcadc72398fd2e6de
Wei Kin Huang <weikin.huang04@gmail.com>
2024-05-05 11:39:45 -0400

    git: default to main branch, plugins: update lesspipe to lesspipe.sh

--- plugins/10-lesspipe.sh
+++ plugins/10-lesspipe.sh
@@ -1,9 +1,9 @@
 # shellcheck shell=bash
 
 # @see https://github.com/wofr06/lesspipe
-if ! command -v lesspipe &>/dev/null; then
+if ! command -v lesspipe.sh &>/dev/null; then
   return
 fi
 
 # shellcheck source=/dev/null
-source <(SHELL=/bin/bash lesspipe 2>/dev/null)
+source <(SHELL=/bin/bash lesspipe.sh 2>/dev/null)
